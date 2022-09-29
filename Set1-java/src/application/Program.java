574b8da898f90e6dc9bbb6adb798919ccd556d78
KiosaninMarc <marcelomarcweb@gmail.com>
2022-09-29 18:09:29 -0300

    Exercício resolvido (Set)

--- Set1-java/src/application/Program.java
+++ Set1-java/src/application/Program.java
@@ -1,10 +1,55 @@
 package application;
+/*Um site de internet registra um log de acessos dos usuários. Um
+registro de log consiste no nome de usuário (apenas uma palavra) e o
+instante em que o usuário acessou o site no padrão ISO 8601,
+separados por espaço, conforme exemplo. Fazer um programa que leia
+o log de acessos a partir de um arquivo, e daí informe quantos usuários
+distintos acessaram o site.*/
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.Set;
+
+import entities.LogEntry;
 
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		//System.out.print("Enter file full path: ");
+		String path = "d:\\in.txt";
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(path))){
+			
+			Set<LogEntry> set = new HashSet<>();
+			
+			String line = br.readLine();//Ler a primeira linha do arquivo
+			while (line != null) {
+				
+				String[] fields = line.split(" ");
+				String username = fields[0];
+				Date moment = Date.from(Instant.parse(fields[1]));
+				
+				set.add(new LogEntry(username, moment));
+				
+				line = br.readLine();	
+				
+			}
+			System.out.println("Total users: " + set.size());
+			System.out.println("Log: " + set.toString());
+			
+		}catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		sc.close();
 	}
 
 }
