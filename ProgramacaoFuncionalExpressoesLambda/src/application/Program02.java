edeb76fac6c62c941ee33c489f7e3d1f6ffd9a76
KiosaninMarc <marcelomarcweb@gmail.com>
2022-10-20 17:20:16 -0300

    Transparência referencial

---
+++ ProgramacaoFuncionalExpressoesLambda/src/application/Program02.java
@@ -0,0 +1,30 @@
+//Transparência referencial
+package application;
+
+import java.util.Arrays;
+
+public class Program02 {
+
+	public static int globalValue = 10;
+	
+	public static void main(String[] args) {
+		
+		int[] vect = new int[] {3, 4, 5};
+		changeOddValues(vect);
+		System.out.println(Arrays.toString(vect));
+	}
+	
+		//Essa função não tem transparencia referencial
+		//Transparencia Referencial -> (Quando o resultado da função depende exclusivamente dos valores de entrada)
+		public static void changeOddValues(int[] numbers) {
+			for (int i = 0; i < numbers.length; i++) {
+				if (numbers[i] % 2 != 0) {
+					numbers[i] += globalValue;
+				}
+			}
+		}
+	}
+
+
+//Uma função possui transparência referencial se seu resultado for sempre o mesmo
+//para os mesmos dados de entrada. Benefícios: simplicidade e previsibilidade.
