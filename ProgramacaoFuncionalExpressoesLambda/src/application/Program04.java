6026a609ea49704b1f195a06ca372f8d944788ba
KiosaninMarc <marcelomarcweb@gmail.com>
2022-10-20 18:24:21 -0300

    Expressao lambda declarada

--- ProgramacaoFuncionalExpressoesLambda/src/application/Program04.java
+++ ProgramacaoFuncionalExpressoesLambda/src/application/Program04.java
@@ -1,14 +1,19 @@
 package application;
-//Predicate - Implementação de interface
+//Expressao lambda declarada
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.function.Predicate;
 
 import entities.Product;
 
 public class Program04 {
 
 	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 		
 		List<Product> list = new ArrayList<>();
 		
@@ -16,16 +21,20 @@ public class Program04 {
 		list.add(new Product("Mouse", 50.00));
 		list.add(new Product("Tablet", 350.00));
 		list.add(new Product("HD Case", 80.90));
+		
+		System.out.print("Digite o valor minimo: ");
+		double min = sc.nextDouble();
+		
+		Predicate<Product> pred = p -> p.getPrice() >= min;
 
-		list.removeIf(Product::nonStaticProductPredicate/*Method reference*/);
-		//Coloco o nome da classe onde o metodo está :: e o nome do metodo
-		//Isso tambem é aceito no sistema lambda do java
+		list.removeIf(pred);
 		
 		for (Product p : list) {
 			System.out.println(p);
 		}
+		
+		sc.close();
 	}
-
 }
 
 //Fazer um programa que, a partir de uma lista de produtos, remova da
