28eca3f44a792f79a129ecdc29e02355b4832c13
KiosaninMarc <marcelomarcweb@gmail.com>
2022-10-21 11:02:09 -0300

    Reference method com metodo estatico

--- ProgramacaoFuncionalExpressoesLambda/src/application/Program05.java
+++ ProgramacaoFuncionalExpressoesLambda/src/application/Program05.java
@@ -4,8 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
-
-import entities.Product;
+import entities.Product02;
 import util.PriceUpdate;
 
 //Consumer (exemplo com forEach)
@@ -16,14 +15,14 @@ public class Program05 {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		List<Product> list = new ArrayList<>();
+		List<Product02> list = new ArrayList<>();
 		
-		list.add(new Product("TV", 900.00));
-		list.add(new Product("Mouse", 50.00));
-		list.add(new Product("Tablet", 350.50));
-		list.add(new Product("HD Case", 80.90));
+		list.add(new Product02("TV", 900.00));
+		list.add(new Product02("Mouse", 50.00));
+		list.add(new Product02("Tablet", 350.50));
+		list.add(new Product02("HD Case", 80.90));
 		
-		list.forEach(new PriceUpdate());
+		list.forEach(Product02::staticPriceUpdate);
 
 		list.forEach(System.out::println);
 	}
--- ProgramacaoFuncionalExpressoesLambda/src/util/PriceUpdate.java
+++ ProgramacaoFuncionalExpressoesLambda/src/util/PriceUpdate.java
@@ -1,13 +1,12 @@
 package util;
 
 import java.util.function.Consumer;
+import entities.Product02;
 
-import entities.Product;
-
-public class PriceUpdate implements Consumer<Product> {
+public class PriceUpdate implements Consumer<Product02> {
 
 	@Override
-	public void accept(Product p) {
+	public void accept(Product02 p) {
 		p.setPrice(p.getPrice() * 1.1);		
 	}	
 }
